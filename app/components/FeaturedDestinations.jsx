8342172073113bdf7434e3be3aac267a814e0c53
DaniNorth <112745991+DaniNorth@users.noreply.github.com>
2024-09-07 14:00:53 -0700

    updates font color to darker grey

--- app/components/FeaturedDestinations.jsx
+++ app/components/FeaturedDestinations.jsx
@@ -1,4 +1,3 @@
-import { Star } from "@mui/icons-material";
 import {
   Card,
   CardMedia,
@@ -18,29 +17,29 @@ const FeaturedDestinations = () => {
           variant="h3"
           sx={{
             textAlign: "center",
-            color: "#46515A",
+            color: "#2f2f2f", // Darker grey color
             fontWeight: 500,
             mt: 1,
             mb: 3,
           }}
         >
-          Featured Destinations
+          Instant Itineraries Where Ever You Are
         </Typography>
-        <Grid container spacing={4}>
+        <Grid container spacing={15}>
           <Grid item xs={12} sm={6} md={4}>
             <Card
               sx={{
-                width: "300px",
+                width: "400px",
                 height: "400px",
                 position: "relative",
                 border: "1px solid black",
-                boxShadow: "rgba(255,255,255,0.25",
+                boxShadow: "rgba(255,255,255,0.25)",
                 borderRadius: "12px",
               }}
             >
-              <CardMedia sx={{ width: "100%", height: "100%" }}>
+              <CardMedia>
                 <img
-                  src="/assets/smp.jpeg"
+                  src="/assets/Boston.png"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               </CardMedia>
@@ -49,41 +48,42 @@ const FeaturedDestinations = () => {
                   position: "absolute",
                   bottom: 0,
                   left: 0,
-                  padding: "16px",
+                  padding: "0", // Remove padding from content
                   backgroundColor: "rgba(0,0,0,0)",
                   width: "100%",
                 }}
               >
-                <Typography variant="h6" sx={{ color: "#fff" }}>
-                  Santa Monica Pier, <br></br>Los Angeles, CA
-                </Typography>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)", // 50% black background
+                    padding: "1px 0", // 1px top and bottom
+                    width: "100%", // Full width container
+                    textAlign: "left", // left align the text
+                    paddingLeft: "6px", // space before text
                   }}
                 >
-                  <Star sx={{ color: "gold", mr: 1 }} />
-                  <Typography sx={{ color: "#fff" }}> 4.3 (697)</Typography>
+                  <Typography variant="h6" sx={{ color: "#fff" }}>
+                    Boston,<br></br>Massachusetts
+                  </Typography>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
+
           <Grid item xs={12} sm={6} md={4}>
             <Card
               sx={{
-                width: "300px",
+                width: "400px",
                 height: "400px",
                 position: "relative",
                 border: "1px solid black",
-                boxShadow: "rgba(255,255,255,0.25",
+                boxShadow: "rgba(255,255,255,0.25)",
                 borderRadius: "12px",
               }}
             >
               <CardMedia sx={{ width: "100%", height: "100%" }}>
                 <img
-                  src="/assets/kyoto.jpg"
+                  src="/assets/Okinawa.png"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               </CardMedia>
@@ -92,41 +92,42 @@ const FeaturedDestinations = () => {
                   position: "absolute",
                   bottom: 0,
                   left: 0,
-                  padding: "16px",
+                  padding: "0", // Remove padding
                   backgroundColor: "rgba(0,0,0,0)",
                   width: "100%",
                 }}
               >
-                <Typography variant="h6" sx={{ color: "#fff" }}>
-                  Kyoto, <br></br>Japan
-                </Typography>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)", // 50% black background
+                    padding: "1px 0", // 1px top and bottom
+                    width: "100%", // Full width container
+                    textAlign: "left", // left align the text
+                    paddingLeft: "6px", // space before text
                   }}
                 >
-                  <Star sx={{ color: "gold", mr: 1 }} />
-                  <Typography sx={{ color: "#fff" }}> 4.6 (1193)</Typography>
+                  <Typography variant="h6" sx={{ color: "white" }}>
+                    Okinawa,<br></br> Japan
+                  </Typography>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
+
           <Grid item xs={12} sm={6} md={4}>
             <Card
               sx={{
-                width: "300px",
+                width: "400px",
                 height: "400px",
                 position: "relative",
                 border: "1px solid black",
-                boxShadow: "rgba(255,255,255,0.25",
+                boxShadow: "rgba(255,255,255,0.25)",
                 borderRadius: "12px",
               }}
             >
               <CardMedia sx={{ width: "100%", height: "100%" }}>
                 <img
-                  src="/assets/lv.jpg"
+                  src="/assets/Salesforce Park.png"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               </CardMedia>
@@ -135,41 +136,42 @@ const FeaturedDestinations = () => {
                   position: "absolute",
                   bottom: 0,
                   left: 0,
-                  padding: "16px",
+                  padding: "0", // Remove padding
                   backgroundColor: "rgba(0,0,0,0)",
                   width: "100%",
                 }}
               >
-                <Typography variant="h6" sx={{ color: "#fff" }}>
-                  Las Vegas <br></br>Neveda
-                </Typography>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)", // 50% black background
+                    padding: "1px 0", // 1px top and bottom
+                    width: "100%", // Full width container
+                    textAlign: "left", // left align the text
+                    paddingLeft: "6px", // space before text
                   }}
                 >
-                  <Star sx={{ color: "gold", mr: 1 }} />
-                  <Typography sx={{ color: "#fff" }}> 4.2 (6930)</Typography>
+                  <Typography variant="h6" sx={{ color: "#fff" }}>
+                    San Francisco <br></br>California
+                  </Typography>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
+
           <Grid item xs={12} sm={6} md={4}>
             <Card
               sx={{
-                width: "300px",
+                width: "400px",
                 height: "400px",
                 position: "relative",
                 border: "1px solid black",
-                boxShadow: "rgba(255,255,255,0.25",
+                boxShadow: "rgba(255,255,255,0.25)",
                 borderRadius: "12px",
               }}
             >
               <CardMedia sx={{ width: "100%", height: "100%" }}>
                 <img
-                  src="/assets/kauai.jpg"
+                  src="/assets/la jolla.png"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               </CardMedia>
@@ -178,41 +180,42 @@ const FeaturedDestinations = () => {
                   position: "absolute",
                   bottom: 0,
                   left: 0,
-                  padding: "16px",
+                  padding: "0", // Remove padding
                   backgroundColor: "rgba(0,0,0,0)",
                   width: "100%",
                 }}
               >
-                <Typography variant="h6" sx={{ color: "#fff" }}>
-                  Kauai, <br></br>Hawaii
-                </Typography>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)", // 50% black background
+                    padding: "1px 0", // 1px top and bottom
+                    width: "100%", // Full width container
+                    textAlign: "left", // left align the text
+                    paddingLeft: "6px", // space before text
                   }}
                 >
-                  <Star sx={{ color: "gold", mr: 1 }} />
-                  <Typography sx={{ color: "#fff" }}> 4.4 (2893)</Typography>
+                  <Typography variant="h6" sx={{ color: "#fff" }}>
+                    La Jolla, <br></br>California
+                  </Typography>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
+
           <Grid item xs={12} sm={6} md={4}>
             <Card
               sx={{
-                width: "300px",
+                width: "400px",
                 height: "400px",
                 position: "relative",
                 border: "1px solid black",
-                boxShadow: "rgba(255,255,255,0.25",
+                boxShadow: "rgba(255,255,255,0.25)",
                 borderRadius: "12px",
               }}
             >
               <CardMedia sx={{ width: "100%", height: "100%" }}>
                 <img
-                  src="/assets/paris.jpg"
+                  src="/assets/Alamo Square.png"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               </CardMedia>
@@ -221,41 +224,42 @@ const FeaturedDestinations = () => {
                   position: "absolute",
                   bottom: 0,
                   left: 0,
-                  padding: "16px",
+                  padding: "0", // Remove padding
                   backgroundColor: "rgba(0,0,0,0)",
                   width: "100%",
                 }}
               >
-                <Typography variant="h6" sx={{ color: "#fff" }}>
-                  Paris,<br></br>France
-                </Typography>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)", // 50% black background
+                    padding: "1px 0", // 1px top and bottom
+                    width: "100%", // Full width container
+                    textAlign: "left", // left align the text
+                    paddingLeft: "6px", // space before text
                   }}
                 >
-                  <Star sx={{ color: "gold", mr: 1 }} />
-                  <Typography sx={{ color: "#fff" }}> 4.7 (5633)</Typography>
+                  <Typography variant="h6" sx={{ color: "#fff" }}>
+                    San Francisco,<br></br>California
+                  </Typography>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
+
           <Grid item xs={12} sm={6} md={4}>
             <Card
               sx={{
-                width: "300px",
+                width: "400px",
                 height: "400px",
                 position: "relative",
                 border: "1px solid black",
-                boxShadow: "rgba(255,255,255,0.25",
+                boxShadow: "rgba(255,255,255,0.25)",
                 borderRadius: "12px",
               }}
             >
               <CardMedia sx={{ width: "100%", height: "100%" }}>
                 <img
-                  src="/assets/ny.jpg"
+                  src="/assets/Walla Walla.png"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               </CardMedia>
@@ -264,27 +268,28 @@ const FeaturedDestinations = () => {
                   position: "absolute",
                   bottom: 0,
                   left: 0,
-                  padding: "16px",
+                  padding: "0", // Remove padding
                   backgroundColor: "rgba(0,0,0,0)",
                   width: "100%",
                 }}
               >
-                <Typography variant="h6" sx={{ color: "#fff" }}>
-                  Manhatten, <br></br>New York
-                </Typography>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)", // 50% black background
+                    padding: "1px 0", // 1px top and bottom
+                    width: "100%", // Full width container
+                    textAlign: "left", // left align the text
+                    paddingLeft: "6px", // space before text
                   }}
                 >
-                  <Star sx={{ color: "gold", mr: 1 }} />
-                  <Typography sx={{ color: "#fff" }}> 4.4 (4392)</Typography>
+                  <Typography variant="h6" sx={{ color: "#fff" }}>
+                    Walla Walla, <br></br>Washington
+                  </Typography>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
+
         </Grid>
       </Container>
     </div>
